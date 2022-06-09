585bd767c6074334ef2f47648db41effea93487a
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-09 08:09:37 +0530

    added: two lines equal or not

--- LineComparison.java
+++ LineComparison.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 import java.util.Scanner;
 public class LineComparison {
 
-    public static void calculateLengthUC1 () {
+    public static void calculateLengthUC1() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter x1: ");
         int x1 = scanner.nextInt();
@@ -14,9 +14,28 @@ public class LineComparison {
         System.out.print("Enter y2: ");
         int y2 = scanner.nextInt();
         double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        System.out.println("Length = " +length);
+        System.out.println("Length of first line = " + length);
+
+        System.out.print("Enter x3: ");
+        int x3 = scanner.nextInt();
+        System.out.print("Enter y3: ");
+        int y3 = scanner.nextInt();
+        System.out.print("Enter x4: ");
+        int x4 = scanner.nextInt();
+        System.out.print("Enter y4: ");
+        int y4 = scanner.nextInt();
+        double length1 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+        System.out.println("Length of second line= " + length1);
+
+        if (length == length1) {
+            System.out.println("Both the lines are equal");
+        } else {
+            System.out.println("Both the lines are not equal");
+        }
     }
+
     public static void main(String[] args) {
         calculateLengthUC1();
     }
 }
+
