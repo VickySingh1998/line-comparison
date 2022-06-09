5867c97af21300618366479c9cdbbca9f3024f1c
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-09 07:45:05 +0530

    line comparison computation

---
+++ LineComparison.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+public class LineComparison {
+
+    public static void calculateLengthUC1 () {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+    public static void main(String[] args) {
+        calculateLengthUC1();
+    }
+}
