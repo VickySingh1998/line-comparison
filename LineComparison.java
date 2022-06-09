000aeaa60100cce0573648689e733d55205d8f01
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-09 07:52:02 +0530

    added: calculation of length between two points

--- LineComparison.java
+++ LineComparison.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 public class LineComparison {
 
     public static void calculateLengthUC1 () {
-        System.out.println("Welcome to Line Comparison Computation Program");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter x1: ");
+        int x1 = scanner.nextInt();
+        System.out.print("Enter y1: ");
+        int y1 = scanner.nextInt();
+        System.out.print("Enter x2: ");
+        int x2 = scanner.nextInt();
+        System.out.print("Enter y2: ");
+        int y2 = scanner.nextInt();
+        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("Length = " +length);
     }
     public static void main(String[] args) {
         calculateLengthUC1();
